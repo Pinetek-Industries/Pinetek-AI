58a6d4771976066f7192f9ec84c66b887a485e0f
zcomer4d <57999801+zcomer4d@users.noreply.github.com>
2020-06-12 13:01:43 -0400

    Added login execute code

--- onStartup.bat
+++ onStartup.bat
@@ -1,5 +1,6 @@
-/function add add_directory
-/add_directory Pinetek-AI.zip Disk
-/run Disk/Language/English/LOAD_ENGLISH.jss
-/open Disk/WELCOME.html
-/run Disk/font.css
+function add add_directory
+add_directory Pinetek-AI.zip Disk
+run Disk/Language/English/LOAD_ENGLISH.jss
+open Disk/WELCOME.html
+run Disk/font.css
+run Disk/User.cmd
